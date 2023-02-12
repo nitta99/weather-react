d8345610a5f0c7f9e22ad2f7eb7057c26058d55e
nitta99 <93244791+nitta99@users.noreply.github.com>
2023-02-12 12:50:23 +0900

    見た目、DropDown.jsxの修正 検索ボタン：MUIの使用 ドロップダウン：App.jsxに移動

--- src/components/SeachButton.jsx
+++ src/components/SeachButton.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { GetWeather } from "./GetWeather";
+import { Button as MUIButton } from "@mui/material";
 
 export const SeachButton = () => {
   return (
-    <>
-      <input type="submit" value="検索" onClick={GetWeather} />
-    </>
+    <MUIButton
+      variant="contained"
+      size="small"
+      sx={{ marginLeft: 1 }}
+      onClick={GetWeather}
+      type="submit"
+    >
+      検索
+    </MUIButton>
   );
 };
